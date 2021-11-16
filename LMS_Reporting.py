af5e52a1b332d4ebbaf75efa2c5444c6d16b0f73
mjadgit <94192554+mjadgit@users.noreply.github.com>
2021-11-15 23:32:45 -0500

    Add files via upload

---
+++ LMS_Reporting.py
@@ -0,0 +1,24 @@
+from simple_salesforce import Salesforce
+import easygui as ui
+# import csv
+
+msg = "LMS Reporting.\nEnter Org1 LMS ID. Tool Returns Record ID"
+title = "LMS Record ID"
+fieldNames = ["LMS ID:"]
+fieldValue = []
+fieldValue = ui.multenterbox(msg, title, fieldNames)
+
+
+sforg1 = Salesforce(username='REDACTED', password='REDACTED',
+                    security_token='REDACTED')
+
+
+LMS_query = "SELECT Stage__c, Id FROM Project__c WHERE LMS_Id__c = '" + str(fieldValue[0]) + "'"
+# print(LMS_query)
+or1_record_id = sforg1.query(LMS_query)
+
+print(or1_record_id)
+
+ui.msgbox(or1_record_id)
+
+raise SystemExit
